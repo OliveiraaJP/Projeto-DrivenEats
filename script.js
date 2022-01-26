84b98d9a72ddadf166841dbac7c246bc6021e63b
JoaoPaulo <oliveirajpufrj@gmail.com>
2022-01-26 11:23:27 -0300

    Menu pratos

---
+++ script.js
@@ -0,0 +1,18 @@
+function escolhamiojo() {
+    document.getElementById("miojo").style.borderColor = "green";
+    document.getElementById("torrada").style.borderColor = "white";
+    document.getElementById("tofu").style.borderColor = "white";
+}
+
+function escolhatofu() {
+    document.getElementById("tofu").style.borderColor = "green";
+    document.getElementById("miojo").style.borderColor = "white";
+    document.getElementById("torrada").style.borderColor = "white";
+}
+
+function escolhatorrada() {
+    document.getElementById("torrada").style.borderColor = "green";
+    document.getElementById("tofu").style.borderColor = "white";
+    document.getElementById("miojo").style.borderColor = "white";
+}
+
