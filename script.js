9bdc0ab184a87f6abcc764623464a5f4e8097676
JoaoPaulo <oliveirajpufrj@gmail.com>
2022-01-29 19:05:25 -0300

    Marcadores e Botao Footer

--- script.js
+++ script.js
@@ -1,59 +1,88 @@
-function escolhamiojo() {
-    document.getElementById("miojo").style.borderColor = "green";
-    document.getElementById("torrada").style.borderColor = "white";
-    document.getElementById("tofu").style.borderColor = "white";
+let verificarPratos = null;
+let verificarBebidas = null;
+let verificarSobremesas = null;
+
+let precoPrato = 0;
+let precoBebida = 0;
+let precoSobremesa = 0;
+
+
+
+function escolherPrato(classPrato, nomePrato) {
+    const selecionado = document.querySelector(".pratos .selecionado");
+    const selecionadoIcone = document.querySelector(`${classPrato} ion-icon`);
+    if (selecionado !== null) {
+        selecionado.classList.remove("selecionado");
+        selecionadoIcone.classList.add("escondido");
+    }
+
+    const bordaVerde = document.querySelector(classPrato);
+    bordaVerde.classList.add("selecionado");
+
+    const iconeVerde = document.querySelector(`${classPrato} .menu__card-checked`);
+    iconeVerde.classList.remove("escondido");
+    verificarPratos = nomePrato
+    liberarPedido();
+
 }
 
-function escolhatofu() {
-    document.getElementById("tofu").style.borderColor = "green";
-    document.getElementById("miojo").style.borderColor = "white";
-    document.getElementById("torrada").style.borderColor = "white";
-}
-
-function escolhatorrada() {
-    document.getElementById("torrada").style.borderColor = "green";
-    document.getElementById("tofu").style.borderColor = "white";
-    document.getElementById("miojo").style.borderColor = "white";
-}
 
 //--------------------------//
 
-function escolhaagua() {
-    document.getElementById("agua").style.borderColor = "green";
-    document.getElementById("cerveja").style.borderColor = "white";
-    document.getElementById("refri").style.borderColor = "white";
+function escolherBebida(classBebida, nomeBebida) {
+    const selecionado = document.querySelector(".bebidas .selecionado");
+    const selecionadoIcone = document.querySelector(`${classBebida} ion-icon`);
+    if (selecionado !== null) {
+        selecionado.classList.remove("selecionado");
+        selecionadoIcone.classList.add("escondido");
+    }
+    // document.querySelector(".pratos .menu__card classPrato").style.boxShadow = "none";
+
+    const bordaVerde = document.querySelector(classBebida);
+    bordaVerde.classList.add("selecionado");
+
+    const iconeVerde = document.querySelector(`${classBebida} .menu__card-checked`);
+    iconeVerde.classList.remove("escondido");
+
+    verificarBebidas = nomeBebida
+    liberarPedido();
 }
 
-function escolhacerveja() {
-    document.getElementById("cerveja").style.borderColor = "green";
-    document.getElementById("agua").style.borderColor = "white";
-    document.getElementById("refri").style.borderColor = "white";
-}
-
-function escolharefri() {
-    document.getElementById("refri").style.borderColor = "green";
-    document.getElementById("agua").style.borderColor = "white";
-    document.getElementById("cerveja").style.borderColor = "white";
-}
 
 //-----------------------------//
 
-function escolhabolo() {
-    document.getElementById("bolo").style.borderColor = "green";
-    document.getElementById("gelatina").style.borderColor = "white";
-    document.getElementById("frutas").style.borderColor = "white";
-}
+function escolherSobremesa(classSobremesa, nomeSobremesa) {
+    const selecionado = document.querySelector(".sobremesas .selecionado");
+    const selecionadoIcone = document.querySelector(`${classSobremesa} ion-icon`);
+    if (selecionado !== null) {
+        selecionado.classList.remove("selecionado")
+        selecionadoIcone.classList.add("escondido");
+    }
+    // document.querySelector(".pratos .menu__card classPrato").style.boxShadow = "none";
 
-function escolhagelatina() {
-    document.getElementById("gelatina").style.borderColor = "green";
-    document.getElementById("bolo").style.borderColor = "white";
-    document.getElementById("frutas").style.borderColor = "white";
-}
 
-function escolhafrutas() {
-    document.getElementById("frutas").style.borderColor = "green";
-    document.getElementById("bolo").style.borderColor = "white";
-    document.getElementById("gelatina").style.borderColor = "white";
+    const bordaVerde = document.querySelector(classSobremesa);
+    bordaVerde.classList.add("selecionado");
+
+    const iconeVerde = document.querySelector(`${classSobremesa} .menu__card-checked`);
+    iconeVerde.classList.remove("escondido");
+
+    verificarSobremesas = nomeSobremesa
+    liberarPedido();
 }
 
 //-----------------------------//
+function liberarPedido() {
+    if (verificarPratos !== null && verificarBebidas !== null && verificarSobremesas !== null) {
+        const botao = document.querySelector(".avancar");
+        botao.classList.add('liberado');
+        botao.innerHTML = "Fechar Pedido"
+    }
+}
+
+function avancarPedido() {
+    if ((verificarPratos !== null && verificarBebidas !== null && verificarSobremesas !== null)) {
+        prompt('oi');
+    }
+}
+
